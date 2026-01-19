973a4453e652ca4e1f29d2ade4aa8d850c59ae76
Jan Kočiš <jan.kocis@tul.cz>
2026-01-19 22:37:38 +0100

    Beta version 1.0

--- test/app/src/main/java/com/example/ukol14/Task.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ukol14
-
-data class Task(
-    val id: String = "",           // Firestore ID
-    val title: String = "",         // Název úkolu
-    val isCompleted: Boolean = false  // Je hotový?
-)
